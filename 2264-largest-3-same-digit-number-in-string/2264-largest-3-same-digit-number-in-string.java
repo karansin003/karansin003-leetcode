735cde5f4373e66823066c4db762179f891e31bf
Karan Kumar <karansin8672@gmail.com>
2026-07-04 21:05:31 +0530

    Time: 3 ms (63.25%), Space: 43.6 MB (61.18%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -1,48 +1,21 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        if(num.contains("999"))
-        {
-            return  "999";
+        String answer = "";
+
+        for (int i = 0; i <= num.length() - 3; i++) {
+            char first = num.charAt(i);
+            char second = num.charAt(i + 1);
+            char third = num.charAt(i + 2);
+
+            if (first == second && second == third) {
+                String current = num.substring(i, i + 3);
+
+                if (answer.isEmpty() || current.compareTo(answer) > 0) {
+                    answer = current;
+                }
+            }
         }
-        else if(num.contains("888"))
-        {
-            return "888";
-        }
-        else if(num.contains("777"))
-        {
-            return "777";
-        }
-        else if(num.contains("666"))
-        {
-            return "666";
-        }
-        else if(num.contains("555"))
-        {
-            return "555";
-        }
-        else if(num.contains("444"))
-        {
-            return "444";
-        }
-        else if(num.contains("333"))
-        {
-            return "333";
-        }
-        else if(num.contains("222"))
-        {
-            return "222";
-        }
-        else if(num.contains("111"))
-        {
-            return "111";
-        }
-        else if(num.contains("000"))
-        {
-            return "000";
-        }
-        else
-        {
-            return "";
-        }
+
+        return answer;
     }
 }
