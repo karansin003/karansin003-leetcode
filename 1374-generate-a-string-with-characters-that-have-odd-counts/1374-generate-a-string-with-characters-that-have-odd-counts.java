495360203531d7daef017e3ab6242a1bd0e3423a
Karan Kumar <karansin8672@gmail.com>
2026-07-05 08:15:37 +0530

    Time: 0 ms (100%), Space: 43.1 MB (24.09%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -1,16 +1,11 @@
 class Solution {
     public String generateTheString(int n) {
-        StringBuilder sb = new StringBuilder();
-        if (n % 2 == 1) {
-            for (int i = 0; i < n; i++) {
-                sb.append('a');
-            }
-        } else {
-            for (int i = 0; i < n - 1 ; i++) {
-                sb.append('a');
-            }
-            sb.append('b');
+        char ch[] = new char[n];
+        Arrays.fill(ch,'a');
+        if(n % 2 == 0)
+        {
+            ch[n-1] = 'b';
         }
-        return sb.toString();
+        return new String(ch);
     }
 }
