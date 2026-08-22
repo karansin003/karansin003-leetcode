fcc7efc3a5430359485ec06bc881ccf3b6ed1ca4
Karan Kumar <karansin8672@gmail.com>
2026-08-22 17:20:57 +0530

    Update 3622-check-divisibility-by-digit-sum-and-product.java

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean checkDivisibility(int n) {
-        int sum = 0,product = 1;
+        int sum = 0, product = 1;
         int temp = n;
-        while(temp != 0)
-        {
+        while (temp != 0) {
             sum += temp % 10;
             product *= temp % 10;
             temp /= 10;
