9e487c1566655d898824d7dae80fb608de292ea9
Karan Kumar <karansin8672@gmail.com>
2026-09-26 19:19:33 +0530

    Time: 4 ms (76.9%), Space: 43.4 MB (58.78%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,9 +1,10 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int arr[] = new int[heights.length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = heights[i];
-        }
+        // int arr[] = new int[heights.length];
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i] = heights[i];
+        // }
+        int arr[] = heights.clone();
         Arrays.sort(arr);
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
